55b3539602189edc89c205bf6f4127d85f3290b2
刘勗 <liuxu@liuxudeMacBook-Air.local>
2022-09-08 23:18:20 +0800

    第一次提交

---
+++ java_ds/Function/src/TestMax.java
@@ -0,0 +1,16 @@
+public class TestMax {
+    public static void main(String[] args) {
+        int i =5;
+        int j =2;
+        int k =max(i,j);
+        System.out.println("最大值"+i+"和"+j+"是"+k);
+    }
+    public static int max(int num1,int num2){
+        int result;
+        if(num1>num2)
+            return num1;
+        else
+            result=num2;
+        return result;
+    }
+}
